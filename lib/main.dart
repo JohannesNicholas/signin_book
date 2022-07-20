7f3c40879eb0dcb41b431c78ec78d5526118cd0b
JohannesNicholas <45587025+Goanna7007@users.noreply.github.com>
2022-07-20 13:49:28 +1000

    Basic style setup

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,13 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Row(
+          children: [
+            Text(widget.title),
+            Text("Mount Nelson station"),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
